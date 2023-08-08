29fdd0c36aceb7311814dd041293b554b317a888
puchochiri <doawishfor@naver.com>
2023-08-08 20:39:13 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_chool_check_geolocator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_chool_check_geolocator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_chool_check_geolocator/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
